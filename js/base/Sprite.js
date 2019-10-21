670165af8429e6112f3c04973948e94b0c2fc818
用户名 <>
2019-10-21 17:24:18 +0800

    第二天结束 主要内容 设计背景图,地板,水管的类将其在页面上绘制出来,小鸟类能够切换,水平移动

--- js/base/Sprite.js
+++ js/base/Sprite.js
@@ -3,10 +3,10 @@ import { DataStore } from "./DataStore.js";
 // 各种图片的父类
 
 export class Sprite{
-    constructor(img=null,srcX=0,srcY=0,srcW=0,srcH=0,x=0,y=0,width=0,heigth=0){
-        // 获取变量值
-        const dataSorte = DataStore.getInstance();
-        this.ctx = DataStore.ctx;
+    constructor(img=null,srcX=0,srcY=0,srcW=0,srcH=0,x=0,y=0,width=0,height=0){
+        // 获取变量池
+        const dataStore = DataStore.getInstance();
+        this.ctx = dataStore.ctx;
         this.img = img; // 图片
         this.srcX = srcX; // 图片的起始x坐标
         this.srcY = srcY; //起始Y的坐标
@@ -15,11 +15,11 @@ export class Sprite{
         this.x = x; // 在画图上的x坐标
         this.y = y; // 在画图上的y坐标
         this.width = width; // 画的宽度
-        this.heigth = heigth; // 画的高度
+        this.height = height; // 画的高度
     }
     // 画图
-    draw(img=this.img,srcX=this.srcX,srcY=this.srcY,srcW=this.srcW,srcH=this.srcH,x=this.x,y=this.y,width=this.width,heigth=this.heigth){
-        this.ctx.drawImage(img,srcX,srcY,srcW,srcH,x,y,width,heigth);
+    draw(img=this.img,srcX=this.srcX,srcY=this.srcY,srcW=this.srcW,srcH=this.srcH,x=this.x,y=this.y,width=this.width,height=this.height){
+        this.ctx.drawImage(img,srcX,srcY,srcW,srcH,x,y,width,height);
     }
     // 获取指定图片
     static getImage(key){
